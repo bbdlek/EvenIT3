cbac90e7f3d0de6be9e7a9067d8615af2fc72968
bbdlek <bbdlek@gmail.com>
2023-01-11 00:27:00 +0900

    dd

--- Assets/0_EvenIT3/1_Scripts/8_Global/DBManagerScript.cs
+++ Assets/0_EvenIT3/1_Scripts/8_Global/DBManagerScript.cs
@@ -1,9 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.Networking;
+
+using Data = Google.Apis.Sheets.v4.Data;
 
 public class DBManagerScript : Singleton<DBManagerScript>
 {
@@ -17,17 +22,36 @@ public class DBManagerScript : Singleton<DBManagerScript>
         
     }
 
+    private SheetsService sheetsService;
+
     private void Start()
     {
-        BaseClientService.Initializer initializer = new BaseClientService.Initializer();
-        initializer.ApiKey = "AIzaSyBkbHNmywT0HvikSLPZpAD9jMhGsVuxOlY";
+        sheetsService = new SheetsService(new BaseClientService.Initializer
+        {
+            HttpClientInitializer = GetCredential(),
+            ApplicationName = "SheetTest",
+        });
+
+        string spreadsheetId = "1u1h-5dEZUSupamAgJ_fJEN3vbqc6RB7sgfkOjaiZGJg";
+        
+        List<Data.Request> requests = new List<Data.Request>();
+        Data.BatchUpdateSpreadsheetRequest requestBody = new Data.BatchUpdateSpreadsheetRequest();
+        requestBody.Requests = requests;
+
+        SpreadsheetsResource.BatchUpdateRequest request =
+            sheetsService.Spreadsheets.BatchUpdate(requestBody, spreadsheetId);
+
+        Data.BatchUpdateSpreadsheetResponse response = request.Execute();
+        
+        Debug.Log(JsonConvert.SerializeObject(response));
+        //StartCoroutine(ObtainSheetData());
     }
-    
+
     IEnumerator ObtainSheetData()
     {
         UnityWebRequest www =
             UnityWebRequest.Get(
-                "https://drive.google.com/uc?export=download&id=1u1h-5dEZUSupamAgJ_fJEN3vbqc6RB7sgfkOjaiZGJg");
+                "https://sheets.googleapis.com/v4/spreadsheets/1u1h-5dEZUSupamAgJ_fJEN3vbqc6RB7sgfkOjaiZGJg/values/SheetTest?key=AIzaSyBkbHNmywT0HvikSLPZpAD9jMhGsVuxOlY");
         yield return www.SendWebRequest();
         if (www.isNetworkError || www.isHttpError)
         {
@@ -35,10 +59,15 @@ public class DBManagerScript : Singleton<DBManagerScript>
         }
         else
         {
-            string json = www.downloadHandler.text.Replace("[", "");
+            string json = JsonConvert.SerializeObject(www.downloadHandler.text);
             json.Replace("]", "");
             Debug.Log(json);
         }
+    }
+
+    public static UserCredential GetCredential()
+    {
+        return null;
     }
 
 }
