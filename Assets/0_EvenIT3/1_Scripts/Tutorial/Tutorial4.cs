045d16b1476622742b82e8d99dd9b39b4d850242
ika0216 <40966618+ika0216@users.noreply.github.com>
2023-01-27 05:11:06 +0900

    튜토리얼 4

--- Assets/0_EvenIT3/1_Scripts/Tutorial/Tutorial4.cs
+++ Assets/0_EvenIT3/1_Scripts/Tutorial/Tutorial4.cs
@@ -5,26 +5,8 @@ using UnityEngine.UI;
 
 public class Tutorial4 : MonoBehaviour
 {
-    public GameObject T4View1, T4View2, T4View3, T4View4, T4View5, T4View6, T4View7, T4View8, T4View9, T4View10, T4View11, T4View12, T4View13, T4View14, T4View15, T4View16, T4View17, T4View18;
-    public Image T4Btn1, T4Btn2, T4Btn3, T4Btn4, T4Btn5, T4Btn6, T4Btn7, T4Btn8, vBtn9, vBtn10, T4Btn11, T4Btn12, T4Btn13, T4Btn14, T4Btn15, T4Btn16, T4Btn17, T4Btn18;
-
-    public void T4Click1()
-    {
-        T4View1.SetActive(false);
-        T4View2.SetActive(true);
-    }
-
-    public void T4Click2()
-    {
-        T4View2.SetActive(false);
-        T4View3.SetActive(true);
-    }
-
-    public void T4Click3()
-    {
-        T4View3.SetActive(false);
-        T4View4.SetActive(true);
-    }
+    public GameObject T4View4, T4View5, T4View6, T4View7, T4View8, T4View9, T4View10, T4View11, T4View12, T4View13, T4View14, T4View15, T4View16, T4View17, T4View18;
+    public Image T4Btn4, T4Btn5, T4Btn6, T4Btn7, T4Btn8, vBtn9, vBtn10, T4Btn11, T4Btn12, T4Btn13, T4Btn14, T4Btn15, T4Btn16, T4Btn17, T4Btn18;
 
     public void T4Click4()
     {
